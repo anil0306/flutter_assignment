768d3e891c0816bc5bba8db41c37e54fb6ed88eb
Anil Kumar <anilkumar@Anils-MacBook-Air.local>
2025-05-17 08:36:05 +0530

    added entities data

--- lib/data/models/album_model.dart
+++ lib/data/models/album_model.dart
@@ -1,10 +1,13 @@
-class AlbumModel {
-  final int id;
-  final String title;
+import 'package:flutter_assignment/domain/entities/album.dart';
 
-  AlbumModel({required this.id, required this.title});
+class AlbumModel extends Album {
+  const AlbumModel({required super.id, required super.title});
 
   factory AlbumModel.fromJson(Map<String, dynamic> json) {
     return AlbumModel(id: json['id'] as int, title: json['title'] as String);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'id': id, 'title': title};
   }
 }
--- lib/data/models/photo_model.dart
+++ lib/data/models/photo_model.dart
@@ -1,15 +1,31 @@
-class PhotoModel {
-  final int id;
-  final String title;
-  final String url;
+import 'package:flutter_assignment/domain/entities/photo.dart';
 
-  PhotoModel({required this.id, required this.title, required this.url});
+class PhotoModel extends Photo {
+  const PhotoModel({
+    required super.id,
+    required super.albumId,
+    required super.title,
+    required super.url,
+    required super.thumbnailUrl,
+  });
 
   factory PhotoModel.fromJson(Map<String, dynamic> json) {
     return PhotoModel(
-      id: json['id'] as int,
-      title: json['title'] as String,
-      url: json['url'] as String,
+      id: json['id'],
+      albumId: json['albumId'],
+      title: json['title'],
+      url: json['url'],
+      thumbnailUrl: json['thumbnailUrl'],
     );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'albumId': albumId,
+      'title': title,
+      'url': url,
+      'thumbnailUrl': thumbnailUrl,
+    };
   }
 }
